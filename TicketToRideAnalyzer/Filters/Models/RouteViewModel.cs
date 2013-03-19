22dbed29517d360a2f44970ab5b26b398c97a73e
Joe Beernink <jabeerni@yahoo.com>
2013-03-18 21:52:06 -0700

    Accidentally dragged and dropped files in UI
    Not sure what happened here

---
+++ TicketToRideAnalyzer/Filters/Models/RouteViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using RouteAnalyzer;
+
+namespace TicketToRideAnalyzer.Models
+{
+    public class RouteViewModel
+    {
+        public string StartCity { get; set; }
+        public string EndCity { get; set; }
+
+        public List<City> StartCities { get; set; }
+        public List<City> EndCities { get; set; }
+
+        public List<string> RouteCities { get; set; }
+        public int routeCost { get; set; }
+
+        public IEnumerable<SelectListItem> StartingCities
+        {
+            get { return StartCities.Select(c => new SelectListItem { Value = c.ToString(), Text = c.ToString() }); }
+        }
+
+        public IEnumerable<SelectListItem> EndingCities
+        {
+            get { return EndCities.Select(c => new SelectListItem { Value = c.ToString(), Text = c.ToString() }); }
+        }
+
+    }
+}
